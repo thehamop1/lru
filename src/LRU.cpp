6d13a56d96c030d9e944c73e5cffcb6c4713f758
thehamop1 <cristian.gutierrez.56@my.csun.edu>
2022-05-15 20:57:58 +0000

    src

--- src/LRU.cpp
+++ src/LRU.cpp
@@ -8,7 +8,7 @@
 
 #include "LRU.hpp"
 
-PriorityExpiryCache::PriorityExpiryCache(){
+PriorityExpiryCache::PriorityExpiryCache():m_maxItems(0){
 };
 
 PriorityExpiryCache::PriorityExpiryCache(unsigned int size): m_maxItems(size)
@@ -36,15 +36,28 @@ void PriorityExpiryCache::Set(std::string key, CacheData value, int priority, in
   m_nameLookup.insert({key, lruValue});
 
   auto priorityCategory = m_priorityLookup.find(lruValue->m_priority);
-
-  //theres no current priority category for this type
   if(priorityCategory == m_priorityLookup.end()){
+    //theres no current priority category for this type
     //list is empty in this case
     auto priorityIt = m_priorityLookup.insert({lruValue->m_priority, std::list<std::shared_ptr<LRU_VALUE>>()}).first;
     auto listIt = priorityIt->second.insert(priorityIt->second.begin(), lruValue);
     lruValue->m_priorityIt = listIt;
   }else{
-    
+    //list is not empty here
+    auto listIt = priorityCategory->second.insert(priorityCategory->second.begin(), lruValue);
+    lruValue->m_priorityIt = listIt;
+  }
+
+
+  auto timeoutCategory = m_timeoutLookup.find(lruValue->m_timeout);
+  if(timeoutCategory == m_timeoutLookup.end()){
+    auto timeoutIt = m_timeoutLookup.insert({lruValue->m_timeout, std::list<std::shared_ptr<LRU_VALUE>>()}).first;
+    auto listIt = timeoutIt->second.insert(timeoutIt->second.begin(), lruValue);
+    lruValue->m_timeoutIt = listIt;
+  }else{
+    //list is not empty here
+    auto listIt = timeoutCategory->second.insert(priorityCategory->second.begin(), lruValue);
+    lruValue->m_priorityIt = listIt;
   }
 
   EvictItems();
@@ -55,15 +68,19 @@ void PriorityExpiryCache::SetMaxItems(int numItems) {
   EvictItems();
 };
 
+/**
+ * @brief Prints all the keys in the cache for debugging purposes
+ * 
+ */
 void PriorityExpiryCache::DebugPrintKeys() {
-  // ... the interviewee does not need to implement this now.
-  // Prints all the keys in the cache for debugging purposes
   std::cout << "LRU Values: " << std::endl;
   for(const auto& val : m_nameLookup){
-    std::cout << "{" << val.first << ", " << val.second << "}," << std::endl;
+    std::cout << "\t{" << val.first << ", " << val.second->m_value << "}," << std::endl;
   }
 };
 
 void PriorityExpiryCache::EvictItems() {
   // TODO(interviewee): implement this
+  if(m_nameLookup.size()<m_maxItems) return;
+
 };
