a2645ae7051f9edda7b238e41440b4d50e6a6885
thehamop1 <cristian.gutierrez.56@my.csun.edu>
2022-05-15 19:43:56 +0000

    getting the launch working

--- src/LRU.cpp
+++ src/LRU.cpp
@@ -22,26 +22,28 @@ CacheData* PriorityExpiryCache::Get(std::string key) {
   // ... the interviewee does not need to implement this now.
   // Assume that this will return the value for the key in the cache
   return nullptr;
-}
+};
 
 void PriorityExpiryCache::Set(std::string key, CacheData value, int priority, int expiryInSecs) {
   // ... the interviewee does not need to implement this now.
   // Assume that this will add this key, value pair to the cache
-  
   EvictItems();
-}
+};
 
 void PriorityExpiryCache::SetMaxItems(int numItems) {
   maxItems = numItems;
   EvictItems();
-}
+};
 
 void PriorityExpiryCache::DebugPrintKeys() {
   // ... the interviewee does not need to implement this now.
   // Prints all the keys in the cache for debugging purposes
-}
+  std::cout << "LRU Values: " << std::endl;
+  for(const auto& val : m_nameLookup){
+    std::cout << "{" << val.first << ", " << val.second << "}," << std::endl;
+  }
+};
 
 void PriorityExpiryCache::EvictItems() {
   // TODO(interviewee): implement this
-  
-}
+};
