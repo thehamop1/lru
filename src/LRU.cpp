00650aee4cb5c969e9dd4d469ac44a878a071075
thehamop1 <cristian.gutierrez.56@my.csun.edu>
2022-05-15 20:21:56 +0000

    starting to get things in the list

--- src/LRU.cpp
+++ src/LRU.cpp
@@ -11,27 +11,47 @@
 PriorityExpiryCache::PriorityExpiryCache(){
 };
 
-PriorityExpiryCache::PriorityExpiryCache(unsigned int size): maxItems(size)
+PriorityExpiryCache::PriorityExpiryCache(unsigned int size): m_maxItems(size)
 {
-  m_nameLookup.reserve(maxItems);//grab memory ahead of time
-  m_priorityLookup.reserve(maxItems);
-  m_timeoutLookup.reserve(maxItems);
+  m_nameLookup.reserve(m_maxItems);//grab memory ahead of time
+  m_priorityLookup.reserve(m_maxItems);
+  m_timeoutLookup.reserve(m_maxItems);
 };
 
 CacheData* PriorityExpiryCache::Get(std::string key) {
   // ... the interviewee does not need to implement this now.
   // Assume that this will return the value for the key in the cache
-  return nullptr;
+  auto f = m_nameLookup.find(key);
+  if(f==m_nameLookup.end()){
+    std::cout << "Did not find the value in the cache!" << std::endl;
+    return nullptr;
+  }
+
+  return &((f->second).get()->m_value);
 };
 
 void PriorityExpiryCache::Set(std::string key, CacheData value, int priority, int expiryInSecs) {
-  // ... the interviewee does not need to implement this now.
   // Assume that this will add this key, value pair to the cache
+  std::shared_ptr<LRU_VALUE> lruValue = std::make_shared<LRU_VALUE>(key, value, priority, expiryInSecs);
+  m_nameLookup.insert({key, lruValue});
+
+  auto priorityCategory = m_priorityLookup.find(lruValue->m_priority);
+
+  //theres no current priority category for this type
+  if(priorityCategory == m_priorityLookup.end()){
+    //list is empty in this case
+    auto priorityIt = m_priorityLookup.insert({lruValue->m_priority, std::list<std::shared_ptr<LRU_VALUE>>()}).first;
+    auto listIt = priorityIt->second.insert(priorityIt->second.begin(), lruValue);
+    lruValue->m_priorityIt = listIt;
+  }else{
+    
+  }
+
   EvictItems();
 };
 
 void PriorityExpiryCache::SetMaxItems(int numItems) {
-  maxItems = numItems;
+  m_maxItems = numItems;
   EvictItems();
 };
 
